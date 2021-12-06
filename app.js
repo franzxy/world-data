e8da916c7d8648b4e445348fac5f5dc0d198035b
timontrettin <timon.trettin@icloud.com>
2021-12-06 17:42:50 +0100

    switched to Node and added GET requests to backend

---
+++ app.js
@@ -0,0 +1,109 @@
+// DO NOT CHANGE!
+//init app with express, util, body-parser, csv2json
+const express = require('express');
+const app = express();
+const sys = require('util');
+const path = require('path');
+const bodyParser = require('body-parser');
+const csvConverter = require('csvtojson');
+
+//register body-parser to handle json from res / req
+app.use( bodyParser.json() );
+
+//register public dir to serve static files (html, css, js)
+app.use( express.static( path.join(__dirname, "public") ) );
+
+// END DO NOT CHANGE!
+
+
+/**************************************************************************
+****************************** csvtojson *********************************
+**************************************************************************/
+let data
+
+// convert csv to json and write to data
+csvConverter()
+  .fromFile("world_data.csv")
+  .then((json) => {
+    data = json
+  })
+
+/**************************************************************************
+********************** handle HTTP METHODS ***********************
+**************************************************************************/
+// GET
+
+app.get('/items', (req, res) => {
+  return res.send(data);
+});
+
+app.get('/items/:id', (req, res) => {
+  let result = data.find(item => item.id === req.params.id)
+  if (result) {
+    return res.send(result)
+  } else {
+    return res.send(`No such id ${req.params.id} in database.`);
+  }
+});
+
+app.get('/items/:id1/:id2', (req, res) => {
+  let input1 = data.find(item => item.id === req.params.id1)
+  let input2 = data.find(item => item.id === req.params.id2)
+  let result = data.filter(item => {
+    return item.id > req.params.id1 && item.id < req.params.id2
+  })
+  if (input1 && input2 && result) {
+    return res.send(result)
+  } else {
+    return res.send('Range not possible.');
+  }
+});
+
+app.get('/properties', (req, res) => {
+  return res.send(... new Map(
+    data
+      // get property name
+      .map(a => Object.keys(a))
+      // only keep one instance of each property
+      .map(v => [JSON.stringify(v), v])
+    ).values()
+  );
+});
+
+app.get('/properties/:num', (req, res) => {
+  let result = [... new Map(
+    data
+      // get property name
+      .map(a => Object.keys(a))
+      // only keep one instance of each property
+      .map(v => [JSON.stringify(v), v])
+    ).values()][0][req.params.num]
+  if (result) {
+    return res.send(result)
+  } else {
+    return res.send('No such property available.');
+  }
+});
+
+// POST
+
+app.post('/items', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+// DELETE
+
+app.delete('/items', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.delete('/items/id', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+// DO NOT CHANGE!
+// bind server localhost to port 3000
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ public/js/ajax.js
@@ -0,0 +1,1 @@
+alert("No ajax calls and handlers implemented yet! Check your ajax.js under public/js.");
